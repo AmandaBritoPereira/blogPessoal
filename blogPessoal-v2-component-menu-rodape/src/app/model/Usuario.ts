fd69cf69873ff35ff39825fdaffb69414b2ffd67
unknown <amandabritopereira@outlook.com>
2021-08-01 12:38:37 -0300

    Adicionado rotas de entrar e cadastrar criadas

---
+++ blogPessoal-v2-component-menu-rodape/src/app/model/Usuario.ts
@@ -0,0 +1,11 @@
+import { Postagem } from "./postagem"
+
+export class Usuario {
+    public idUsuario: number
+    public nome: string
+    public email: string
+    public senha: string
+    public foto: string
+    public tipo: string
+    public postagem: Postagem[]
+}
