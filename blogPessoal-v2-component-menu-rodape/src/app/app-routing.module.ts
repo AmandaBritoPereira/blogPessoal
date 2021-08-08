35e5bf955793a96fa60a1acccf3d434f254fe324
unknown <amandabritopereira@outlook.com>
2021-08-08 12:49:55 -0300

    Componente inicio sem consumo

--- blogPessoal-v2-component-menu-rodape/src/app/app-routing.module.ts
+++ blogPessoal-v2-component-menu-rodape/src/app/app-routing.module.ts
@@ -4,18 +4,23 @@ import { CadastrarComponent } from './cadastrar/cadastrar.component';
 import { EntrarComponent } from './entrar/entrar.component';
 import { InicioComponent } from './inicio/inicio.component';
 
+// >>> Rotas do site
 const routes: Routes = [
 
-  {path: '', redirectTo: 'entrar', pathMatch:'full'},
+  {path: '', redirectTo: 'entrar', pathMatch: 'full'},
 
-{path:'entrar', component: EntrarComponent},
+{path: 'entrar', component: EntrarComponent},
 {path: 'cadastrar', component:CadastrarComponent},
 {path: 'inicio', component: InicioComponent}
 
 ]; 
+// <<< Rotas do site
 
+// >>> Bibliotecas das rotas
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+// <<< Bibliotecas das rotas
+
 export class AppRoutingModule { }
--- blogPessoal-v2-component-menu-rodape/src/app/app.component.spec.ts
+++ blogPessoal-v2-component-menu-rodape/src/app/app.component.spec.ts
@@ -23,7 +23,7 @@ describe('AppComponent', () => {
   it(`should have as title 'blogPessoal'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('blogPessoal');
+    expect(app.title).toEqual('blogPessoal'); //erro devido retirada do HelloWord
   });
 
   it('should render title', () => {
